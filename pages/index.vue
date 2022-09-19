4fc08ad0f93a23cd5663dea284f7f22818779401
Roy Rutjens <royrutjens@hotmail.com>
2022-09-19 14:21:03 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ jsonData }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  async asyncData({ route, store }) {
+    const data = await fetch(`${process.env.BASE_URL}/countries.json`).then(
+      (res) => res.json()
+    );
+    return { jsonData: data };
+  },
+  data() {
+    return {
+      jsonData: {},
+    };
+  },
+};
+</script>
