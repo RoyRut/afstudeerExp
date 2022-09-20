22a4da9475e8ffdfa45accf258a548af2cdc9a20
Roy Rutjens <royrutjens@hotmail.com>
2022-09-20 15:23:14 +0200

    sorteren tabal

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <div class="colorBG">
     <p v-for="name in names">{{ name.value }}</p>
   </div>
 </template>
+<style lang="sass" scoped>
 
+</style>>
+
+</style>
 <script>
 export default {
   name: "IndexPage",
@@ -11,20 +15,27 @@ export default {
     const data = await fetch(`${process.env.BASE_URL}/data.json`).then((res) =>
       res.json()
     );
-    const headerNames = data.sheets.IP_LOANS_0.colHeaderData.dataTable[0];
-    return { jsonData: data, names: headerNames };
+    const headerNames = data.sheets.IP_LOANS_0.colHeaderData.dataTable[0]
+    const valueSet =  data.sheets.IP_LOANS_0.data.dataTable
+  
+    return { jsonData: data, names: headerNames, valueSet, };
   },
   data() {
     return {
       jsonData: [],
       names: [],
+      valueZero: [], 
     };
   },
   mounted() {
-    const names = this.jsonData.sheets.IP_LOANS_0.colHeaderData.dataTable[0];
-
-    // console.log(names);
-    return { names: names };
+    console.log(this.valueSet);
+    for(let i = 0; i < this.names.length; i++){
+      console.log(this.names[i].value)
+      for(let x = 0; x < this.valueSet.length; x++) {
+        console.log(this.valueSet[x][i].value)
+      }
+    }
+    
   },
 };
 </script>
