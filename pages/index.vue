bdf690ab6d9c557ff45cad9a10bd5f5b482aaeb3
Roy Rutjens <royrutjens@hotmail.com>
2022-09-21 09:40:54 +0200

    instal p5 whit example

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="colorBG">
+    <div class="d-flex justify-content-center" id="p5Canvas"></div>
+    <div class="message d-flex justify-content-center">
+      {{message}}
+    </div>
+
+
     <p v-for="name in names">{{ name.value }}</p>
   </div>
 </template>
@@ -9,33 +15,57 @@
 
 </style>
 <script>
+  if (process.browser) {
+  var radar = require('@/assets/js/Radar.js')
+}
+
 export default {
   name: "IndexPage",
+
   async asyncData({ route, store }) {
     const data = await fetch(`${process.env.BASE_URL}/data.json`).then((res) =>
       res.json()
     );
+
     const headerNames = data.sheets.IP_LOANS_0.colHeaderData.dataTable[0]
     const valueSet =  data.sheets.IP_LOANS_0.data.dataTable
-  
-    return { jsonData: data, names: headerNames, valueSet, };
+    return { jsonData: data, names: headerNames, valueSet, }
   },
   data() {
     return {
       jsonData: [],
       names: [],
-      valueZero: [], 
+      valueSet: [], 
+      message: "" ,
     };
   },
+
   mounted() {
-    console.log(this.valueSet);
+    const P5 = require('p5')
+    new P5(radar.main)
+    
+    radar.setDelegate(this.callbackOnP5);
+
     for(let i = 0; i < this.names.length; i++){
       console.log(this.names[i].value)
+
       for(let x = 0; x < this.valueSet.length; x++) {
         console.log(this.valueSet[x][i].value)
       }
     }
     
   },
+
+  methods: {
+    callbackOnP5: function(timeStr) {
+      this.message = timeStr;
+    }
+  }
 };
 </script>
+
+<style>
+.message {
+  color: black;
+}
+</style>
